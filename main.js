3cdf60102009838731fbcba2a5576abd41bfc300
FekyDEV <71899549+FekyDEV@users.noreply.github.com>
2022-01-29 21:25:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /**
 * @name Multi-Language Support
-* @description Made for Discord bots.
+* @description Multi-Language support made for Discord bots. (JavaScript)
 * @author FekyDEV
 * @authorId 603505971507101698
 * @version 1.0.0
@@ -8,5 +8,36 @@
 * @source https://github.com/FekyDEV/multi-language/new/main
 * @license MIT
 */
-/*@cc_on
-@if (@_jscript)
+
+// MULTI-LANGUAGE SUPPORT
+function translate(what) {
+    let lan = "sk"
+    let translation = ""
+    let data_locale = require(`./${lan}.json`)
+    let en_data_locale = require(`./en.json`)
+
+    let keyys = Object.keys(en_data_locale)
+    let keyys_len = keyys.length
+    let done = ""
+
+    for (let i = 0; i < keyys_len; i++) {
+        let kej = eval(`en_data_locale.${keyys[i]}.${what}`) || false
+
+        if(kej !== false) {
+                 done = keyys[i]
+        } else {
+            continue;
+        }
+
+    }
+
+    try{
+        translation = eval(`data_locale.${done}.${what}`)
+            return translation;
+    } catch {
+        translation = eval(`en_data_locale.${done}.${what}`)
+            return translation;
+    }
+
+}
+// END (MULTI-LANGUAGE SUPPORT)
