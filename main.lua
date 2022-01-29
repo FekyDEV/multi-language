dc2cec117c89404c9120f8802da370dfe9fe4618
FekyDEV <71899549+FekyDEV@users.noreply.github.com>
2022-01-29 21:23:14 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+/**
+* @name Multi-Language Support
+* @description Made for Discord bots.
+* @author FekyDEV
+* @authorId 603505971507101698
+* @version 1.0.0
+* @invite sKKEyUn
+* @source https://github.com/FekyDEV/multi-language/new/main
+* @license MIT
+*/
+/*@cc_on
+@if (@_jscript)
