034d8a36f49530de015cd240de106466d0d47b08
FekyDEV <71899549+FekyDEV@users.noreply.github.com>
2022-02-03 15:44:28 +0100

    Updated ;)

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 /**
-* @name Multi-Language Support
-* @description Multi-Language support made for Discord bots. (JavaScript)
-* @author FekyDEV
-* @authorId 603505971507101698
-* @version 1.0.0
-* @invite sKKEyUn
-* @source https://github.com/FekyDEV/multi-language/new/main
-* @license MIT
-*/
+ * @name Multi-Language Support
+ * @description Multi-Language support made for Discord bots. (JavaScript)
+ * @author FekyDEV
+ * @authorId 603505971507101698
+ * @version 1.1.0
+ * @invite sKKEyUn
+ * @source https://github.com/FekyDEV/multi-language/new/main
+ * @license MIT
+ */
 
 // MULTI-LANGUAGE SUPPORT
 function translate(what) {
     let lan = "sk"
-    let translation = ""
     let data_locale = require(`./${lan}.json`)
     let en_data_locale = require(`./en.json`)
 
@@ -23,17 +22,18 @@ function translate(what) {
     for (let i = 0; i < keyys_len; i++) {
         let kej = eval(`en_data_locale.${keyys[i]}.${what}`) || false
 
-        if(kej !== false) {
-            done = keyys[i]
-        }
+            if(kej) {
+                done = keyys[i]
+            }
+
     }
 
-    if(!done) return translation = "`error`";
+    if(!done) return "`error`";
 
     try{
-        return translation = eval(`data_locale.${done}.${what}`);
+        return eval(`data_locale.${done}.${what}`);
     } catch {
-        return translation = eval(`en_data_locale.${done}.${what}`);
+        return eval(`en_data_locale.${done}.${what}`);
     }
 
 }
